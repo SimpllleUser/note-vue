384a462823574bcf359cc4a85125da70011a2e9f
v.pecheritsya <v.pecheritsya@upstars.com>
2023-08-24 23:56:23 +0300

    INIT project

---
+++ src/components/Editor/BEditor.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { QuillEditor } from '@vueup/vue-quill';
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+</script>
+
+<template>
+  <div>
+    <quill-editor theme="snow" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div><b-editor /></div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import BEditor from 'src/components/Editor/BEditor.vue';
+</script>
