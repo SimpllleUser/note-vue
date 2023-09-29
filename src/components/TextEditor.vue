afe680fc5ac5aadd1829a43fbed621f7cc8217cc
v.pecheritsya <v.pecheritsya@upstars.com>
2023-09-30 01:18:01 +0300

    feat: init new base text editor with dynamic data

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,101 +1,129 @@
 <template>
   <div class="full-width">
-    <div ref="editorRef" class="editor"></div>
-    <button @click="saveContent">Save Content</button>
-    <div v-html="savedContent" class="output"></div>
+    <div ref="editorRef" class="editor" id="editorjs"></div>
   </div>
 </template>
 
-<script>
-import { ref, onMounted } from 'vue';
-import EditorJS from '@editorjs/editorjs';
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import EditorJS, {API} from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
 import LinkTool from '@editorjs/link';
 import SimpleImage from '@editorjs/simple-image';
 import Checklist from '@editorjs/checklist';
-import Paragraph  from '@editorjs/paragraph';
-import Table  from '@editorjs/table';
+import Paragraph from '@editorjs/paragraph';
+import Table from '@editorjs/table';
 import InlineCode from '@editorjs/inline-code';
-import Marker  from '@editorjs/marker';
+import Marker from '@editorjs/marker';
 import ColorPlugin from 'editorjs-text-color-plugin';
+import RawTool from '@editorjs/raw';
+import { useVModel } from '@vueuse/core'
+interface Props {
+  modelValue: string
 
-export default {
-  data() {
-    return {
-      editor: null,
-      savedContent: '',
-    };
-  },
-  setup() {
-    const editorRef = ref(null);
+}
 
-    onMounted(() => {
-      const editor = new EditorJS({
-        autofocus: true,
-        holder: editorRef.value,
-        tools: {
-          table: Table,
-          Marker: {
-            class: Marker,
-            shortcut: 'CMD+SHIFT+M',
-          },
-          inlineCode: {
-            class: InlineCode,
-            shortcut: 'SHIFT+C',
-          },
-          paragraph: {
-            class: Paragraph,
-            inlineToolbar: true,
-          },
-          checklist: {
-            class: Checklist,
-            inlineToolbar: true,
-          },
-          image: {
-            class: SimpleImage,
-          },
-          header: {
-            class: Header,
-            inlineToolbar: true,
-          },
-          linkTool: {
-            class: LinkTool,
-          },
-          list: {
-            class: List,
-            inlineToolbar: true,
-          },
-          Color: {
-            class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
-            config: {
-              colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
-              defaultColor: '#FF1300',
-              type: 'text',
-              customPicker: true // add a button to allow selecting any colour
-            }
-          },
+interface Emits {
+  (event: 'update:modelValue', payload: string): void
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: ''
+})
+const emit = defineEmits<Emits>()
+
+const savedContent = useVModel(props, 'modelValue', emit)
+
+const editorRef = ref<HTMLElement | null>(null);
+
+
+onMounted(() => {
+  const editor = new EditorJS({
+    autofocus: true,
+    holder: editorRef.value,
+    data: savedContent.value ? JSON.parse(savedContent.value) : '',
+    tools: {
+      raw: RawTool,
+      table: Table,
+      Marker: {
+        class: Marker,
+        shortcut: 'CMD+SHIFT+M',
+        inlineToolbar: true,
+      },
+      inlineCode: {
+        class: InlineCode,
+        shortcut: 'SHIFT+C',
+        inlineToolbar: true,
+      },
+      paragraph: {
+        class: Paragraph,
+        inlineToolbar: true,
+      },
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+      header: {
+        class: Header,
+        inlineToolbar: true,
+      },
+      linkTool: {
+        class: LinkTool,
+        inlineToolbar: true,
+      },
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+      Color: {
+        class: ColorPlugin,
+        config: {
+          colorCollections: [
+            '#EC7878',
+            '#9C27B0',
+            '#673AB7',
+            '#3F51B5',
+            '#0070FF',
+            '#03A9F4',
+            '#00BCD4',
+            '#4CAF50',
+            '#8BC34A',
+            '#CDDC39',
+            '#FFF',
+          ],
+          defaultColor: '#FF1300',
+          type: 'text',
+          customPicker: true, // add a button to allow selecting any color
         },
-      });
-
-      editorRef.value.editor = editor;
-    });
-
-    return { editorRef };
-  },
-  methods: {
-    saveContent() {
-      this.editorRef.editor
-        .save()
-        .then((outputData) => {
-          this.savedContent = JSON.stringify(outputData, null, 2);
-        })
-        .catch((error) => {
-          console.error('Saving failed: ', error);
-        });
+      },
     },
-  },
+    onInit: (editorInstance: { blocks: { render: (arg0: never[]) => void; }; }) => {
+      console.log('Editor.js is initialized and ready to use');
+
+      editorInstance.blocks.render([
+      ]);
+
+    },
+    onChange(api: API, event: BlockMutationEvent | BlockMutationEvent[]) {
+      saveContent()
+    }
+  });
+
+  editorRef.value!.editor = editor;
+});
+
+const saveContent = () => {
+  editorRef.value!.editor
+    .save()
+    .then((outputData) => {
+      savedContent.value = JSON.stringify(outputData, null, 2);
+    })
+    .catch((error) => {
+      console.error('Saving failed: ', error);
+    });
 };
+
 </script>
 
 <style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <div class="full-width">
-      <text-editor />
-      <b-editor v-if="false" />
+      <text-editor v-model="textEditorValue" />
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
 import BEditor from 'src/components/Editor/BEditor.vue';
 import TextEditor from 'components/TextEditor.vue';
+
+const textEditorValue = ref('')
 
 </script>
